604a54760958b4d5440705553d3f8c2561d66b4d
Joao Paulo Lethier <jplethier@gmail.com>
2020-08-02 22:28:08 -0300

    adds rescue from cancan exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
   load_and_authorize_resource unless: :devise_controller?
+
+  rescue_from Cancan::AccessDenied do |_exception|
+    redirect_to root_path, alert: "Você não tem permissão para acessar essa página"
+  end
 end
