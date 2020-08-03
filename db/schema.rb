8f6035e8a6c8c1f45fa8bb0a8dcb3bb4a9f2dd91
Joao Paulo Lethier <jplethier@gmail.com>
2020-08-02 21:07:27 -0300

    installs devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_233936) do
+ActiveRecord::Schema.define(version: 2020_08_03_000601) do
 
   create_table "tasks", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 2020_07_26_233936) do
     t.integer "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "tasks", "users"
